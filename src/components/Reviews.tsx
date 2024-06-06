b8aef228f57c25adf0c3932038d5b8414604c1f2
datguyaymen <the.aymen.benzarti@gmail.com>
2024-06-06 13:53:35 +0100

    fixed review animation

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,5 +1,7 @@
-"use client";
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
+"use client";
+
 import { HTMLAttributes, useEffect, useRef, useState } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { useInView } from "framer-motion";
@@ -14,8 +16,10 @@ const PHONES = [
   "/testimonials/5.jpg",
   "/testimonials/6.jpg",
 ];
+
 function splitArray<T>(array: Array<T>, numParts: number) {
   const result: Array<Array<T>> = [];
+
   for (let i = 0; i < array.length; i++) {
     const index = i % numParts;
     if (!result[index]) {
@@ -23,8 +27,10 @@ function splitArray<T>(array: Array<T>, numParts: number) {
     }
     result[index].push(array[i]);
   }
+
   return result;
 }
+
 function ReviewColumn({
   reviews,
   className,
@@ -42,19 +48,23 @@ function ReviewColumn({
 
   useEffect(() => {
     if (!columnRef.current) return;
+
     const resizeObserver = new window.ResizeObserver(() => {
       setColumnHeight(columnRef.current?.offsetHeight ?? 0);
     });
+
     resizeObserver.observe(columnRef.current);
+
     return () => {
       resizeObserver.disconnect();
     };
   }, []);
+
   return (
     <div
       ref={columnRef}
       className={cn("animate-marquee space-y-8 py-4", className)}
-      style={{ "--marqee-duration": duration } as React.CSSProperties}
+      style={{ "--marquee-duration": duration } as React.CSSProperties}
     >
       {reviews.concat(reviews).map((imgSrc, reviewIndex) => (
         <Review
@@ -66,6 +76,7 @@ function ReviewColumn({
     </div>
   );
 }
+
 interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
   imgSrc: string;
 }
@@ -84,6 +95,7 @@ function Review({ imgSrc, className, ...props }: ReviewProps) {
     POSSIBLE_ANIMATION_DELAYS[
       Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
     ];
+
   return (
     <div
       className={cn(
@@ -117,23 +129,23 @@ function ReviewGrid() {
             reviews={[...column1, ...column3.flat(), ...column2]}
             reviewClassName={(reviewIndex) =>
               cn({
-                "md:hidden": reviewIndex > +column1.length + column3[0].length,
+                "md:hidden": reviewIndex >= column1.length + column3[0].length,
                 "lg:hidden": reviewIndex >= column1.length,
               })
             }
             msPerPixel={10}
           />
           <ReviewColumn
-            className="hidden md:block"
             reviews={[...column2, ...column3[1]]}
+            className="hidden md:block"
             reviewClassName={(reviewIndex) =>
               reviewIndex >= column2.length ? "lg:hidden" : ""
             }
             msPerPixel={15}
           />
           <ReviewColumn
-            className="hidden md:block"
             reviews={column3.flat()}
+            className="hidden md:block"
             msPerPixel={10}
           />
         </>
@@ -151,8 +163,8 @@ export function Reviews() {
         aria-hidden="true"
         src="/what-people-are-buying.png"
         className="absolute -left-32 top-1/3 hidden select-none xl:block"
-        alt="reviews"
       />
+
       <ReviewGrid />
     </MaxWidthWrapper>
   );
