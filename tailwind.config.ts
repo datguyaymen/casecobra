68d26e10ef3ecd6c78f8b4dda5c742467f093775
datguyaymen <the.aymen.benzarti@gmail.com>
2024-07-03 12:11:06 +0100

    cleared animation color bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,18 +60,27 @@ const config = {
       },
       keyframes: {
         "fade-in": {
-          from: { opacity: "0" },
-          to: { opacity: "1" },
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+          },
         },
         marquee: {
           "100%": {
             transform: "translateY(-50%)",
           },
         },
+        flashing: {
+          "0%, 100%": { opacity: "0.2" },
+          "20%": { opacity: "1" },
+        },
       },
       animation: {
         marquee: "marquee var(--marquee-duration) linear infinite",
-        "fade-in": "fade-in 0.5 linear forwards",
+        "fade-in": "fade-in 0.5s linear forwards",
+        flashing: "flashing 1.4s infinite linear",
       },
     },
   },
